c06bd5a438e25b54fa3dbda2dc6f2ef2729a0c7b
zzeynepkocak <zainapkocak@gmail.com>
2025-12-08 02:12:57 +0300

    Identity ayarlari yapildi ve veritabani baglantisi saglandi

---
+++ BiletCebimde/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BiletCebimde.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BiletCebimde.Data
+{
+    public class AppDbContext : IdentityDbContext<Users>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+      
+        {
+            { }
+        }
+    }
+}
+
---
+++ BiletCebimde/Models/Users.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace BiletCebimde.Models
+{
+    public class Users : IdentityUser
+    {
+      
+
+    }
+}
